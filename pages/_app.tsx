0503ea0d9687acbb95d795590483c1757d8ddf29
Alistair Smith <alistairsmith01@gmail.com>
2020-12-28 15:34:11 +0000

    fix: add default export

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import {AppProps} from "next/app";
 import "tailwindcss/tailwind.css";
 
-export function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
